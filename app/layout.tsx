153661f35023d7f991a03040f4cd7a059b3cf0ab
Jo <107114561+Jo-prg@users.noreply.github.com>
2025-09-18 08:31:06 +0200

    Made sidebar "sticky" using flex and overflow-y

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Sidebar } from "@/components/dashboard/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
+          <div className="flex max-h-screen"> 
+            <Sidebar />
+            <div className="flex-1 overflow-y-auto">{children}</div>            
+          </div>
         </ThemeProvider>
       </body>
     </html>
